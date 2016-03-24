483675f88c170897b80a1f75537906f2db343bae
ylxdzsw <ylxdzsw@gmail.com>
2016-03-24 18:08:49 +0800

    basic tests

--- lab1/nextDate.js
+++ lab1/nextDate.js
@@ -2,18 +2,32 @@
 'use strict'
 
 module.exports = (m, d, y) => {
-	let date = addOneDay(m, d, y)
-    return format(date)
+    if(!checkType(m, d, y) || !checkRange(m, d, y)){
+        return "Error: input should be a valid date between 1990.1.1 ~ 2100.12.31"
+    }
+
+	const date = addOneDay(m, d, y)
+    const weekday = nextWeekDay(m, d, y)
+    return format(date, weekday)
 }
 
 const isLeapYear = (y) => !(y % 4 ? 1 : y % 100 ? 0 : y % 400)
 
+const getYearLength = (y) => isLeapYear(y) ? 366 : 365
+
 const getMonthLength = (m, y) => [31, isLeapYear(y) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m-1]
 
+const checkType = (m, d, y) => {
+    const valid_y = typeof(y) == 'number'
+    const valid_m = typeof(m) == 'number'
+    const valid_d = typeof(d) == 'number'
+    return valid_d && valid_m && valid_y
+}
+
 const checkRange = (m, d, y) => {
-    valid_y = 1900 <= y && y <= 2100
-    valid_m = 1    <= m && m <= 12
-    valid_d = 1    <= d && d <= getMonthLength(m, y)
+    const valid_y = 1900 <= y && y <= 2100
+    const valid_m = 1    <= m && m <= 12
+    const valid_d = 1    <= d && d <= getMonthLength(m, y)
     return valid_d && valid_m && valid_y
 }
 
@@ -23,10 +37,32 @@ const addOneMonth = (m, d, y) => m == 12 ? addOneYear(1, d, y) : [m+1, d, y]
 
 const addOneYear = (m, d, y) => [m, d, y+1]
 
+const nextWeekDay = (m, d, y) => {
+    const catchYear  = now => now < y ? catchYear(now+1)+getYearLength(now) : 0
+    const catchMonth = now => now < m ? catchMonth(now+1)+getMonthLength(now,y) : 0
+    const catchDay   = now => d - now
+
+    const interval   = catchYear(1990) + catchMonth(1) + catchDay(1)
+    return (interval+1) % 7 // note that 1900.1.1 is Monday
+}
+
 const format = (date, weekday, lunar) => {
     const m = date[0]
     const d = date[1]
     const y = date[2]
+    const w = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][weekday]
 
-    return `${y}-${m}-${d}`
+    return `${y}-${m}-${d} ${w}`
 }
+
+// exports some tool functions for white-box test
+module.exports.isLeapYear     = isLeapYear
+module.exports.getYearLength  = getYearLength
+module.exports.getMonthLength = getMonthLength
+module.exports.checkType      = checkType
+module.exports.checkRange     = checkRange
+module.exports.addOneDay      = addOneDay
+module.exports.addOneMonth    = addOneMonth
+module.exports.addOneYear     = addOneYear
+module.exports.nextWeekDay    = nextWeekDay
+module.exports.format         = format
--- lab1/test.js
+++ lab1/test.js
@@ -4,8 +4,75 @@
 const should = require("should")
 const nextDate = require("./nextDate.js")
 
-describe("nextDate", () => {
-	it("should report error when year > 2100", () => {
-		nextDate(2,2,2).should.be.exactly("fuck anyway!")
+describe("black-box", () => {
+    it("should get the right answer for some trival cases", () => {
+        nextDate(3,24,2016).should.be.exactly("2016-3-25 Friday")
+    })
+	it("should report error when inputs are not valid", () => {
+        nextDate('q','a','q').should.startWith("Error")
+        nextDate().should.startWith("Error")
+        nextDate(3.3,true,-14).should.startWith("Error")
 	})
+    it("should report error when inputs are out of range", () => {
+        nextDate(1,1,1800).should.startWith("Error")
+        nextDate(1,1,2101).should.startWith("Error")
+        nextDate(1,32,1997).should.startWith("Error")
+        nextDate(2,29,1997).should.startWith("Error")
+        nextDate(13,30,1997).should.startWith("Error")
+    })
+    it("should recognize leap years", () => {
+        nextDate(2,28,2000).should.startWith("2000-2-29")
+        nextDate(2,28,1900).should.startWith("1900-3-1")
+    })
+})
+
+describe("white-box", () => {
+    describe("isLeapYear", () => {
+        it("mutiple of 4 but not 100 is a leap year", () => {
+            nextDate.isLeapYear(1904).should.be.true()
+            nextDate.isLeapYear(1996).should.be.true()
+        })
+        it("mutiple of 100 but not 400 is not a leap year", () => {
+            nextDate.isLeapYear(1900).should.be.false()
+        })
+        it("mutiple of 400 is a leap year", () => {
+            nextDate.isLeapYear(2000).should.be.true()
+        })
+    })
+    describe("getYearLength", () => {
+
+    })
+    describe("getMonthLength", () => {
+
+    })
+    describe("checkType", () => {
+        it("Integers are valid", () => {
+            nextDate.checkType(2,2,2).should.be.ok()
+        })
+        it("NULLs are not numbers", () => {
+            nextDate.checkType().should.not.be.ok()
+            nextDate.checkType(null, null, null).should.not.be.ok()
+        })
+        it("Strings are not numbers", () => {
+            nextDate.checkType('1st','Jan','1996').should.not.be.ok()
+        })
+    })
+    describe("checkRange", () => {
+
+    })
+    describe("addOneDay", () => {
+
+    })
+    describe("addOneMonth", () => {
+
+    })
+    describe("addOneYear", () => {
+
+    })
+    describe("nextWeekDay", () => {
+
+    })
+    describe("format", () => {
+
+    })
 })
